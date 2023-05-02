a92795237295b691e1d1c02e5b5aa37aa13bc1e2
Blog Minutes <forservercheck@gmail.com>
2023-05-02 20:20:22 +0530

    update: authentication.
    Signed-off-by: Blog Minutes <forservercheck@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,33 @@
 import { type AppType } from "next/app";
-
+import { ClerkProvider } from "@clerk/nextjs";
+import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/nextjs";
+import { useRouter } from "next/router";
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
 
+const publicPages = ["/", "/sign-in/[[...index]]", "/sign-up/[[...index]]"];
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const { pathname } = useRouter();
+
+  const isPublicPage = publicPages.includes(pathname);
+
+  return (
+    <ClerkProvider {...pageProps}>
+      {isPublicPage ? (
+        <Component {...pageProps} />
+      ) : (
+        <>
+          <SignedIn>
+            <Component {...pageProps} />
+          </SignedIn>
+          <SignedOut>
+            <RedirectToSignIn redirectUrl={"/"} />
+          </SignedOut>
+        </>
+      )}
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
