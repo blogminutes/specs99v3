9fdaa52f94a8a2852e78e1b4dcea84386608369a
Blog Minutes <forservercheck@gmail.com>
2023-05-06 10:59:31 +0530

    fix: eslint.
    Signed-off-by: Blog Minutes <forservercheck@gmail.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,7 +29,7 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/no-floating-promises": "warn",
   },
 };
 
