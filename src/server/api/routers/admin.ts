a0166fcc89aad7f3f5419c62aa8692eb772a96db
Blog Minutes <forservercheck@gmail.com>
2023-05-06 11:18:03 +0530

    cleanup.
    Signed-off-by: Blog Minutes <forservercheck@gmail.com>

--- src/server/api/routers/admin.ts
+++ src/server/api/routers/admin.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { client } from "~/utils/square";
+import { client } from "~/server/square";
 
 const { locationsApi } = client;
 
