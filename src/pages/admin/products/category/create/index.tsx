ab77ca995e744effbe6aaf9953318820425f6edd
Blog Minutes <forservercheck@gmail.com>
2023-06-26 18:20:45 +0530

    fix: deployment.
    Signed-off-by: Blog Minutes <forservercheck@gmail.com>

--- src/pages/admin/products/category/create/index.tsx
+++ src/pages/admin/products/category/create/index.tsx
@@ -1,13 +1,8 @@
 import React from "react";
-import CreateProduct from "~/components/admin/products/CreateProduct";
 import AdminLayout from "~/components/admin/shared/AdminLayout";
 
 const CreateCategoryPage = () => {
-  return (
-    <AdminLayout>
-      <CreateProduct />
-    </AdminLayout>
-  );
+  return <AdminLayout>{/* <CreateProduct /> */}</AdminLayout>;
 };
 
 export default CreateCategoryPage;
